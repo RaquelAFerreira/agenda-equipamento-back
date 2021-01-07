85fbd9498ff47ba6bebf086d7e353aa1dc5524b6
RaquelAFerreira <raquelalmferreira@gmail.com>
2021-01-07 19:53:13 -0300

    Adiciona comentário inicial

--- agendaEquipamento/src/main/java/br/com/alterdata/agendaEquipamento/AgendaEquipamentoApplication.java
+++ agendaEquipamento/src/main/java/br/com/alterdata/agendaEquipamento/AgendaEquipamentoApplication.java
@@ -3,6 +3,13 @@ package br.com.alterdata.agendaEquipamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @copyright Alterdata Software
+ * @author Raquel Ferreira
+ * @since 05/01/2021
+ * @description Agendamento de empréstimo de equipamentos para colaboradores da Alterdata
+*/
+
 @SpringBootApplication
 public class AgendaEquipamentoApplication {
 
